9a1f9c695bf3c461635946996a2428ab540557bd
AsithandileNyjah <atyuluu@gmail.com>
2023-12-05 16:57:07 +0000

    Product buttons

--- JavaScript/products.js
+++ JavaScript/products.js
@@ -39,15 +39,94 @@ display.addEventListener('click', function(){
     }
 })
 
-let sortBTN = document.querySelector('[sort]')
+let sortBtn = document.querySelector('[sort]')
+sortBtn.textContent = 'Sort by Price';
+sortBtn.addEventListener('click', function () {
+    sortProductsByPrice();
+});
 
-sortBTN.addEventListener('click', sort)
+// Append the sort button to the display element
+display.appendChild(sortBtn);
 
-function sort (){
-    let products = JSON.parse(localStorage.getItem('products'))
-        price.sort(function(a, b){
-            return
-            b.price - a.price
-        })
+// Function to sort products by price
+function sortProductsByPrice() {
+    // Use if statements to sort products by price (lowest to highest)
+    products.sort(function (a, b) {
+        if (a.price < b.price) {
+            return -1;
+        } else if (a.price > b.price) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+    // Update the display with the sorted products
+    display.innerHTML = products.map(function (item, index) {
+        return `
+            <div id="card" class="container-fluid">
+                <div class="card" style="width: 15rem;">
+                <img id="cardImg" src=${item.url} class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text" style="height: 10rem">${item.description}</p>
+                <a class="btn btn-success">R${item.price}</a>
+                <a href="#" value='${index}' class="btn btn-primary" addToCart>Add to Cart</a>
+                </div>
+                </div>
+            </div>
+        `;
+    }).join('');
 }
 
+let searchBTN = document.querySelector('[search]');
+let searchWords = document.querySelector('[searchInput]');
+
+searchBTN.addEventListener('click', function () {
+    let searchTerm = searchWords.value.toLowerCase();
+    searchProducts(searchTerm);
+});
+
+// Function to search products based on the search term
+function searchProducts(searchTerm) {
+    // Use switch cases for different search criteria
+    switch (avocado) {
+        case 'vanilla':
+            filterAndDisplayProducts('Vanilla Body Butter');
+            break;
+        case 'citrus':
+            filterAndDisplayProducts('Citrus Body Butter');
+            break;
+        case 'avocado':
+            filterAndDisplayProducts('Avocado Body Butter');
+            break;
+        default:
+            // If no specific case matches, display all products
+            displayProducts(products);
+    }
+}
+
+// Function to filter and display products based on the given name
+function filterAndDisplayProducts(productName) {
+    const filteredProducts = products.filter(item => item.name.toLowerCase().includes(productName));
+    displayProducts(filteredProducts);
+}
+
+// Function to display products on the page
+function displayProducts(productsToDisplay) {
+    display.innerHTML = productsToDisplay.map(function (item, index) {
+        return `
+            <div id="card" class="container-fluid">
+                <div class="card" style="width: 15rem;">
+                <img id="cardImg" src=${item.url} class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text" style="height: 10rem">${item.description}</p>
+                <a class="btn btn-success">R${item.price}</a>
+                <a href="#" value='${index}' class="btn btn-primary" addToCart>Add to Cart</a>
+                </div>
+                </div>
+            </div>
+        `;
+    }).join('');
+}
