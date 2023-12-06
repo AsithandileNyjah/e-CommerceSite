78969d6cf4b23c592a3af517ebf8a7a6ae1001ba
AsithandileNyjah <atyuluu@gmail.com>
2023-12-06 09:49:46 +0000

    Search

--- JavaScript/products.js
+++ JavaScript/products.js
@@ -45,9 +45,6 @@ sortBtn.addEventListener('click', function () {
     sortProductsByPrice();
 });
 
-// Append the sort button to the display element
-display.appendChild(sortBtn);
-
 // Function to sort products by price
 function sortProductsByPrice() {
     // Use if statements to sort products by price (lowest to highest)
@@ -79,54 +76,37 @@ function sortProductsByPrice() {
     }).join('');
 }
 
-let searchBTN = document.querySelector('[search]');
-let searchWords = document.querySelector('[searchInput]');
 
-searchBTN.addEventListener('click', function () {
-    let searchTerm = searchWords.value.toLowerCase();
-    searchProducts(searchTerm);
-});
+let searchBTN = document.querySelector('[search]')
+let display2 = document.querySelector('[notFound]')
 
-// Function to search products based on the search term
-function searchProducts(searchTerm) {
-    // Use switch cases for different search criteria
-    switch (avocado) {
-        case 'vanilla':
-            filterAndDisplayProducts('Vanilla Body Butter');
-            break;
-        case 'citrus':
-            filterAndDisplayProducts('Citrus Body Butter');
-            break;
-        case 'avocado':
-            filterAndDisplayProducts('Avocado Body Butter');
-            break;
-        default:
-            // If no specific case matches, display all products
-            displayProducts(products);
+// adding an event listener to the searchBTN with the search function
+searchBTN.addEventListener('click', function(event){
+    event.preventDefault();
+    let searchWord = document.querySelector('[searchInput]').value.toLowerCase();
+    let searchedItem = products.filter(item => item.name.toLowerCase().includes(searchWord));
+    let productNotFound = 'Product Not Found'
+    
+    if (searchWord.length > 0) {
+        // Update the display with the searched products
+        display.innerHTML = searchedItem.map(function (item, index) {
+            return `
+                <div id="card" class="col-md-4 my-4">
+                    <div class="card" style="width: 15rem;">
+                    <img id="cardImg" src=${item.url} class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">${item.name}</h5>
+                    <p class="card-text" style="height: 10rem">${item.description}</p>
+                    <a class="btn btn-success">R${item.price}</a>
+                    <a href="#" value='${index}' class="btn btn-primary" addToCart>Add to Cart</a>
+                    </div>
+                    </div>
+                </div>
+            `;
+        }).join('');
+    } else {
+        // this will display when if the conditions are not met
+        console.log(productNotFound);
+        display2.innerHTML = productNotFound;
     }
-}
-
-// Function to filter and display products based on the given name
-function filterAndDisplayProducts(productName) {
-    const filteredProducts = products.filter(item => item.name.toLowerCase().includes(productName));
-    displayProducts(filteredProducts);
-}
-
-// Function to display products on the page
-function displayProducts(productsToDisplay) {
-    display.innerHTML = productsToDisplay.map(function (item, index) {
-        return `
-            <div id="card" class="container-fluid">
-                <div class="card" style="width: 15rem;">
-                <img id="cardImg" src=${item.url} class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">${item.name}</h5>
-                <p class="card-text" style="height: 10rem">${item.description}</p>
-                <a class="btn btn-success">R${item.price}</a>
-                <a href="#" value='${index}' class="btn btn-primary" addToCart>Add to Cart</a>
-                </div>
-                </div>
-            </div>
-        `;
-    }).join('');
-}
+});
